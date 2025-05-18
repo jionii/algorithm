960be0fadd424dbd738ad175c1756bf1479c235f
jiwon <110143494+jionii@users.noreply.github.com>
2025-05-18 15:20:30 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
