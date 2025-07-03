6e9a399004ef5b95dac263a491c83378472f3230
jiwon <110143494+jionii@users.noreply.github.com>
2025-07-03 16:16:46 +0900

    [level 2] Title: 타겟 넘버, Time: 7.74 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return backtrack(numbers, target, 0, 0);
+    }
+    int backtrack(int[] numbers, int target, int sum, int idx) {
+        if(idx == numbers.length) {
+            return (sum == target) ? 1 : 0;
+        }
+            int count = 0;
+    count += backtrack(numbers, target, sum+numbers[idx], idx+1);
+    count += backtrack(numbers, target, sum-numbers[idx], idx+1);
+    return count;
+    }
+}
